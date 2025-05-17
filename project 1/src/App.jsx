f91ab5c028c0e8882c0118f101505dce1c05b540
rishi200205 <rishirajjaiswal0103@gmail.com>
2025-05-17 15:25:42 +0530

    start reactjs

---
+++ project 1/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './components/card'
+
+function App() {
+
+  return (
+
+      <div>
+        <Card name="love babbar">
+          <h1>hi there</h1>
+          <p>i am starting reactjs</p>
+          <p> let's do it</p>
+        </Card>
+      </div>
+     
+  )
+}
+
+export default App
---
+++ project 1/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default card
